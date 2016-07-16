571c2db68812f2e9802ca3e6e32c4ea8c3d70d8d
Fraser McIntosh <frasermcintosh1@gmail.com>
2016-07-16 09:48:13 +0000

    fix buttonMessage bug

--- app.js
+++ app.js
@@ -117,22 +117,24 @@ function buttonMessage(recipientId, text) {
     if(text === "buttons"){
             
             var message = {
-              "type":"template",
-              "payload":{
-                "template_type":"button",
-                "text":"What do you want to do next?",
-                "buttons":[
-                  {
-                    "type":"web_url",
-                    "url":"https://petersapparel.parseapp.com",
-                    "title":"Show Website"
-                  },
-                  {
-                    "type":"postback",
-                    "title":"Start Chatting",
-                    "payload":"USER_DEFINED_PAYLOAD"
+            "attachment": {
+                  "type":"template",
+                  "payload":{
+                    "template_type": "button",
+                    "text":"What do you want to do next?",
+                    "buttons":[
+                      {
+                        "type":"web_url",
+                        "url":"https://petersapparel.parseapp.com",
+                        "title":"Show Website"
+                      },
+                      {
+                        "type":"postback",
+                        "title":"Start Chatting",
+                        "payload":"USER_DEFINED_PAYLOAD"
+                      }
+                    ]
                   }
-                ]
               }
             };
     
