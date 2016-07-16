e19476752f701ed38fd421422dbaa21eaf504a93
Fraser McIntosh <frasermcintosh1@gmail.com>
2016-07-16 11:37:17 +0000

    debug message function

--- app.js
+++ app.js
@@ -52,7 +52,7 @@ app.post('/webhook', function (req, res) {
 function message(recipientId, message){
     seenMessage(recipientId);
     typingMessage(recipientId);
-    setTimeout(sendMessage(recipientId, message), 5000);
+   // setTimeout(sendMessage(recipientId, message), 5000);
     
 }
 
