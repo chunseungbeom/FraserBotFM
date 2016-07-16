4dff11e2146d2edfd645ef840b784cc9d1d8c2ff
Fraser McIntosh <frasermcintosh1@gmail.com>
2016-07-16 22:50:28 +0000

    increase number of quick reply options with test button

--- app.js
+++ app.js
@@ -221,6 +221,26 @@ function quickRepliesMessage(recipientId, text) {
                 "content_type":"text",
                 "title":"Green",
                 "payload":"DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_GREEN"
+              },
+                  {
+                "content_type":"text",
+                "title":"Red",
+                "payload":"DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_RED"
+              },
+              {
+                "content_type":"text",
+                "title":"Green",
+                "payload":"DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_GREEN"
+              },
+                  {
+                "content_type":"text",
+                "title":"Red",
+                "payload":"DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_RED"
+              },
+              {
+                "content_type":"text",
+                "title":"Green",
+                "payload":"DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_GREEN"
               }
             ]
           }
